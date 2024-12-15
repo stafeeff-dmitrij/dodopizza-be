e877028c6e8f2d105dac59062462dc6b30ba3630
Стафеев Дмитрий <stafeeff.dmitrij@yandex.ru>
2024-12-15 14:05:08 +0300

    perf(django_logging): Подключить стилизованное логирование
    Установить и настроить стилизованное логирование при помощи пакета colorlog

--- src/config/settings/django_logging.py
+++ src/config/settings/django_logging.py
@@ -1,10 +1,22 @@
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+    'formatters': {
+        'color': {
+            '()': 'colorlog.ColoredFormatter',
+            'format': '%(log_color)s%(asctime)-8s %(levelname)s - %(name)s.py | '
+                      'func:%(funcName)s (%(lineno)s) - %(message)s',
+            'log_colors': {
+                'DEBUG': 'white',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'bold_red',
+            },
         },
+    },
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler', 'formatter': 'color'},
     },
     'loggers': {
         'django': {
